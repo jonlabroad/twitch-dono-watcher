d786a87722e135707e0f20b093323d09adae7f29
Jon LaBroad <jon.labroad@gmail.com>
2022-01-23 11:12:21 -0800

    use cloud config

--- src/TwitchDonoWatcher.ts
+++ src/TwitchDonoWatcher.ts
@@ -7,8 +7,7 @@ import StreamElements from "./StreamElements";
 import TwitchClient from "./TwitchClient";
 
 export default class TwitchDonoWatcher {
-    channels: string[];
-    client: tmi.Client;
+    channels: string[] = [];
     twitchClient = new TwitchClient();
     streamInfo: Record<string, any> = {};
     hoagieDbClients: Record<string, HoagieDbClient> = {};
@@ -16,11 +15,7 @@ export default class TwitchDonoWatcher {
     donoTracker: DonoTracker = new DonoTracker(this.hoagieClients);
     historyWritten = new Set<string>();
 
-    constructor(channels: string[]) {
-        this.channels = [...channels];
-        this.client = new tmi.Client({
-            channels
-        });
+    constructor() {
     }
 
     public async run() {
@@ -28,7 +23,7 @@ export default class TwitchDonoWatcher {
         const config = await (new HoagieDbClient("n/a").getConfig());
         this.channels = config?.streamers ?? [];
         console.log(this.channels);
-        this.client = new tmi.Client({
+        const client = new tmi.Client({
             channels: [...this.channels]
         });
 
@@ -51,7 +46,7 @@ export default class TwitchDonoWatcher {
         }, 5000)
 
         const self = this;
-        this.client.on("message", (channel, userstate, message, selfBool) => {
+        client.on("message", (channel, userstate, message, selfBool) => {
             // Look for streamlabs/streamelements dono messages (different for all channels)
             if (userstate["display-name"]?.toLowerCase() === "streamlabs" || userstate["display-name"]?.toLowerCase() === "streamelements") {
                 if (message.includes("$")) {
@@ -75,41 +70,41 @@ export default class TwitchDonoWatcher {
             }
         });
 
-        this.client.on("roomstate", (channel, state) => {
+        client.on("roomstate", (channel, state) => {
             console.log({ channel, state });
         });
 
-        this.client.on("cheer", (channel, userstate, message) => {
+        client.on("cheer", (channel, userstate, message) => {
             console.log({ type: "cheer", user: userstate["display-name"], message });
             console.log(userstate.bits ?? 0);
             const channelName = channel.toLowerCase().replace("#", "");
             self.donoTracker.handleCheer(channelName, self.streamInfo[channelName], userstate);
         })
 
-        this.client.on("subscription", (channel, username, method, message, userstate) => {
+        client.on("subscription", (channel, username, method, message, userstate) => {
             console.log({ type: "subscription", username, message, method });
             const channelName = channel.toLowerCase().replace("#", "");
             self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username);
         })
 
-        this.client.on("resub", (channel, username, method, message, userstate) => {
+        client.on("resub", (channel, username, method, message, userstate) => {
             console.log({ type: "subscription", username, message, method });
             const channelName = channel.toLowerCase().replace("#", "");
             self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username);
         })
 
-        this.client.on("subgift", (channel, username, method, message, userstate) => {
+        client.on("subgift", (channel, username, method, message, userstate) => {
             console.log({ type: "subgift", username, message, method });
             const channelName = channel.toLowerCase().replace("#", "");
             self.donoTracker.handleSubGifts(channelName, self.streamInfo[channelName], username);
         })
 
-        this.client.on("connected", (address, port) => {
+        client.on("connected", (address, port) => {
             console.log("connected");
         });
-        this.client.on("disconnected", (reason) => {
+        client.on("disconnected", (reason) => {
             console.log({ disconnected: reason });
         })
-        this.client.connect();
+        client.connect();
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import TwitchDonoWatcher from "./TwitchDonoWatcher";
 
 console.log({version: 1.0})
-new TwitchDonoWatcher(["andythefrenchy", "thesongery", "joplaysviolin", "andythefrenchy", "jodoesntplayviolin", "hoagieman5000"]).run();
+new TwitchDonoWatcher().run();
