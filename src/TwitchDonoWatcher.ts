ce26e86f534c344fea0d3706632bf180a5c1cdb3
Jon LaBroad <jon.labroad@gmail.com>
2022-01-25 20:45:19 -0800

    no sub tiers?

--- src/TwitchDonoWatcher.ts
+++ src/TwitchDonoWatcher.ts
@@ -88,13 +88,13 @@ export default class TwitchDonoWatcher {
         })
 
         client.on("resub", (channel, username, method, message, userstate, methods) => {
-            console.log({ type: "subscription", username, message, method, methods });
+            console.log({ type: "subscription", username, message, method, plan: methods.plan });
             const channelName = channel.toLowerCase().replace("#", "");
             self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username, methods?.plan ?? "1000");
         })
 
         client.on("subgift", (channel, username, method, message, userstate, methods) => {
-            console.log({ type: "subgift", username, message, method, methods });
+            console.log({ type: "subgift", username, message, method, methods, plan: methods.plan });
             const channelName = channel.toLowerCase().replace("#", "");
             self.donoTracker.handleSubGifts(channelName, self.streamInfo[channelName], username, methods?.plan ?? "1000");
         })
