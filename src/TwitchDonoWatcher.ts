20f1d1dc28b9d1b515ea6556446c9ed8377c8cab
Jon LaBroad <jon.labroad@gmail.com>
2022-07-18 21:01:58 -0700

    keep it running at all costs

--- src/TwitchDonoWatcher.ts
+++ src/TwitchDonoWatcher.ts
@@ -19,93 +19,106 @@ export default class TwitchDonoWatcher {
     }
 
     public async run() {
-        await Secrets.init();
-        const config = await (new HoagieDbClient("n/a").getConfig());
-        this.channels = Array.from(config?.streamers.values() ?? []);
-        console.log(this.channels);
-        const client = new tmi.Client({
-            channels: [...this.channels]
-        });
+        while (true) {
+            try {
+                await Secrets.init();
+                const config = await (new HoagieDbClient("n/a").getConfig());
+                this.channels = Array.from(config?.streamers.values() ?? []);
+                console.log(this.channels);
+                const client = new tmi.Client({
+                    channels: [...this.channels]
+                });
 
-        setInterval(async () => {
-            const streamInfo = await Promise.all(this.channels.map(channel => this.twitchClient.getUserStream(channel)));
-            streamInfo.forEach((info, i) => {
-                this.streamInfo[this.channels[i].toLowerCase()] = info;
-                if (info) {
-                    const dbClient = new HoagieDbClient(this.channels[i]);
-                    this.hoagieDbClients[this.channels[i].toLowerCase()] = dbClient;
-                    const client = new HoagieClient(this.channels[i]);
-                    this.hoagieClients[this.channels[i].toLowerCase()] = client;
-                    if (!this.historyWritten.has(info.id)) {
-                        console.log({newStream: info.id});
-                        dbClient.setStreamHistory(info.id)
-                        this.historyWritten.add(info.id);
-                    }
-                }
-            });
-        }, 5000)
-
-        const self = this;
-        client.on("message", (channel, userstate, message, selfBool) => {
-
-            // Look for streamlabs/streamelements dono messages (different for all channels)
-            if (userstate["display-name"]?.toLowerCase() === "streamlabs" || userstate["display-name"]?.toLowerCase() === "streamelements") {
-                if (message.includes("$")) {
-                    try {
-                        const regex = StreamElements.getDonoRegex(channel);
-                        console.log({ channel, message });
-                        if (regex) {
-                            const matches = message.match(regex);
-
-                            const username = matches?.groups?.username;
-                            const amount = matches?.groups?.amount;
-                            const channelName = channel.toLowerCase().replace("#", "");
-                            if (username && amount) {
-                                self.donoTracker.handleDono(channelName, self.streamInfo[channelName], username, amount);
+                setInterval(async () => {
+                    const streamInfo = await Promise.all(this.channels.map(channel => this.twitchClient.getUserStream(channel)));
+                    streamInfo.forEach((info, i) => {
+                        this.streamInfo[this.channels[i].toLowerCase()] = info;
+                        if (info) {
+                            const dbClient = new HoagieDbClient(this.channels[i]);
+                            this.hoagieDbClients[this.channels[i].toLowerCase()] = dbClient;
+                            const client = new HoagieClient(this.channels[i]);
+                            this.hoagieClients[this.channels[i].toLowerCase()] = client;
+                            if (!this.historyWritten.has(info.id)) {
+                                console.log({ newStream: info.id });
+                                dbClient.setStreamHistory(info.id)
+                                this.historyWritten.add(info.id);
                             }
                         }
-                    } catch (err) {
-                        console.error(err);
+                    });
+                }, 5000)
+
+                const self = this;
+                client.on("message", (channel, userstate, message, selfBool) => {
+
+                    // Look for streamlabs/streamelements dono messages (different for all channels)
+                    if (userstate["display-name"]?.toLowerCase() === "streamlabs" || userstate["display-name"]?.toLowerCase() === "streamelements") {
+                        if (message.includes("$")) {
+                            try {
+                                const regex = StreamElements.getDonoRegex(channel);
+                                console.log({ channel, message });
+                                if (regex) {
+                                    const matches = message.match(regex);
+
+                                    const username = matches?.groups?.username;
+                                    const amount = matches?.groups?.amount;
+                                    const channelName = channel.toLowerCase().replace("#", "");
+                                    if (username && amount) {
+                                        self.donoTracker.handleDono(channelName, self.streamInfo[channelName], username, amount);
+                                    }
+                                }
+                            } catch (err) {
+                                console.error(err);
+                            }
+                        }
                     }
-                }
+                });
+
+                client.on("roomstate", (channel, state) => {
+                    console.log({ channel, state });
+                });
+
+                client.on("cheer", (channel, userstate, message) => {
+                    console.log({ type: "cheer", user: userstate["display-name"], message });
+                    console.log(userstate.bits ?? 0);
+                    const channelName = channel.toLowerCase().replace("#", "");
+                    self.donoTracker.handleCheer(channelName, self.streamInfo[channelName], userstate);
+                })
+
+                client.on("subscription", (channel, username, methods, message, userstate) => {
+                    console.log({ type: "subscription", username, message, methods });
+                    const channelName = channel.toLowerCase().replace("#", "");
+                    self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username, methods?.plan ?? "1000");
+                })
+
+                client.on("resub", (channel, username, method, message, userstate, methods: any) => {
+                    console.log({ type: "subscription", username, message, method, plan: methods['msg-param-sub-plan'] });
+                    const channelName = channel.toLowerCase().replace("#", "");
+                    self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username, methods['msg-param-sub-plan'] ?? "1000");
+                })
+
+                client.on("subgift", (channel, username, method, message, userstate, methods) => {
+                    console.log({ type: "subgift", username, message, method, methods, plan: methods['msg-param-sub-plan'] });
+                    const channelName = channel.toLowerCase().replace("#", "");
+                    self.donoTracker.handleSubGifts(channelName, self.streamInfo[channelName], username, methods['msg-param-sub-plan'] ?? "1000");
+                })
+
+                client.on("connected", (address, port) => {
+                    console.log("connected");
+                });
+                client.on("disconnected", (reason) => {
+                    console.log({ disconnected: reason });
+                })
+                client.connect();
+
+            } catch (err) {
+                console.error(err);
+                await this.sleep(10000);
+                console.log("trying again");
             }
-        });
+        }
+    }
 
-        client.on("roomstate", (channel, state) => {
-            console.log({ channel, state });
-        });
-
-        client.on("cheer", (channel, userstate, message) => {
-            console.log({ type: "cheer", user: userstate["display-name"], message });
-            console.log(userstate.bits ?? 0);
-            const channelName = channel.toLowerCase().replace("#", "");
-            self.donoTracker.handleCheer(channelName, self.streamInfo[channelName], userstate);
-        })
-
-        client.on("subscription", (channel, username, methods, message, userstate) => {
-            console.log({ type: "subscription", username, message, methods });
-            const channelName = channel.toLowerCase().replace("#", "");
-            self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username, methods?.plan ?? "1000");
-        })
-
-        client.on("resub", (channel, username, method, message, userstate, methods: any) => {
-            console.log({ type: "subscription", username, message, method, plan: methods['msg-param-sub-plan'] });
-            const channelName = channel.toLowerCase().replace("#", "");
-            self.donoTracker.handleSub(channelName, self.streamInfo[channelName], username, methods['msg-param-sub-plan'] ?? "1000");
-        })
-
-        client.on("subgift", (channel, username, method, message, userstate, methods) => {
-            console.log({ type: "subgift", username, message, method, methods, plan: methods['msg-param-sub-plan'] });
-            const channelName = channel.toLowerCase().replace("#", "");
-            self.donoTracker.handleSubGifts(channelName, self.streamInfo[channelName], username, methods['msg-param-sub-plan'] ?? "1000");
-        })
-
-        client.on("connected", (address, port) => {
-            console.log("connected");
-        });
-        client.on("disconnected", (reason) => {
-            console.log({ disconnected: reason });
-        })
-        client.connect();
+    private async sleep(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import TwitchDonoWatcher from "./TwitchDonoWatcher";
 
-console.log({version: 1.0})
+console.log({ version: 1.0 })
 new TwitchDonoWatcher().run();
